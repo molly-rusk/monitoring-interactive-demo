cbca082eeb125e1886b767245aad8f27bb8bb17f
Molly Rusk <mollyrusk@gmail.com>
2021-12-16 12:23:07 -0700

    moved rollbar

--- index.js
+++ index.js
@@ -12,8 +12,9 @@ const rollbar = new Rollbar({
 let students = []
 
 const app = express()
+app.use(express.json())
 
-app.use(rollbar.errorHandler())
+
 
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
@@ -31,6 +32,8 @@ app.post('/api/student', (req,res) => {
     res.status(200).send(students)
 })
 
+
 const port = process.env.PORT || 4545
+app.use(rollbar.errorHandler())
 
 app.listen(port, () => console.log(`Take us to warp ${port}`))
